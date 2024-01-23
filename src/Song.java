2d58232864f1aaeaf972aed3ade396bde1e3659b
gravo-lan <alvintsolh@gmail.com>
2024-01-23 08:12:17 +0800

    Finished

--- src/Song.java
+++ src/Song.java
@@ -1,4 +1,28 @@
+import java.util.ArrayList;
+
 public class Song {
 
-    private String
+    private final String title, artist;
+    private final ArrayList<String> listens = new ArrayList<>();
+
+    public Song(String title, String artist) {
+        this.title = title;
+        this.artist = artist;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getArtist() {
+        return artist;
+    }
+
+    public int howMany(ArrayList<String> arrayList) {
+        int original = listens.size();
+        for (String s: arrayList) {
+            if (!listens.contains(s.toLowerCase())) listens.add(s.toLowerCase());
+        }
+        return listens.size()-original;
+    }
 }
